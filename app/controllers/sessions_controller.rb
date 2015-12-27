3f25e1377a089d4a45c1bef337b6b032443a197a
Jason <hinchy@gmail.com>
2015-12-27 12:40:54 +1100

    Add account activations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,22 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #login
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        #login
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      #Error
-      flash.now[:danger] = "Invalid email/password combination" #not quite right!
-      render 'new'
-    end
+        #Error
+        flash.now[:danger] = "Invalid email/password combination" #not quite right!
+        render 'new'
+      end
   end
 
   def destroy
